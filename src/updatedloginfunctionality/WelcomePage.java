c9ea4d9ccef485571bea728b533189339cbf0014
Faris-Faiz <u2000421@siswa.um.edu.my>
2021-12-28 12:11:18 +0800

    Added Search Functionality

--- src/updatedloginfunctionality/WelcomePage.java
+++ src/updatedloginfunctionality/WelcomePage.java
@@ -22,6 +22,7 @@ public class WelcomePage extends javax.swing.JFrame {
     ResultSet rs = null;
     LoginForm1 lf = new LoginForm1();
     static String moduleCode = "PLACEHOLDER!";
+    DefaultTableModel tblModel;
 
     /**
      * Creates new form WelcomePage
@@ -29,6 +30,7 @@ public class WelcomePage extends javax.swing.JFrame {
     public WelcomePage() {
         ConnectDatabase.connectdb();
         initComponents();
+        tblModel = (DefaultTableModel) jTable1.getModel();
         setUsername();
         retrieveData();
     }
@@ -56,7 +58,7 @@ public class WelcomePage extends javax.swing.JFrame {
                 String ACTIVITY = rs.getString("ACTIVITY");
 
                 String tbData[] = {MODULES, CREDIT, ACTIVITY};
-                DefaultTableModel tblModel = (DefaultTableModel) jTable1.getModel();
+                //DefaultTableModel tblModel = (DefaultTableModel) jTable1.getModel();
 
                 tblModel.addRow(tbData);
             }
@@ -76,9 +78,9 @@ public class WelcomePage extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        resetButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,15 +93,14 @@ public class WelcomePage extends javax.swing.JFrame {
             }
         });
         jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField1KeyPressed(evt);
+            }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTextField1KeyReleased(evt);
+            }
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 jTable1keyTypedEvent(evt);
-            }
-        });
-
-        jButton1.setText("🔎");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
             }
         });
 
@@ -119,22 +120,31 @@ public class WelcomePage extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jTable1);
 
+        resetButton.setText("Reset");
+        resetButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                resetButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(161, 161, 161)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(624, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
+                .addGap(387, 387, 387))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addComponent(jTextField1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(resetButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 875, Short.MAX_VALUE)
+                            .addComponent(jTextField1))))
                 .addGap(18, 18, 18))
         );
         layout.setVerticalGroup(
@@ -142,10 +152,10 @@ public class WelcomePage extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGap(76, 76, 76)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                .addGap(42, 42, 42)
+                .addComponent(resetButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
                 .addGap(18, 18, 18))
@@ -153,10 +163,6 @@ public class WelcomePage extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
         JTable source = (JTable) evt.getSource();
@@ -173,26 +179,43 @@ public class WelcomePage extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField1PropertyChange
 
     private void jTable1keyTypedEvent(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTable1keyTypedEvent
-        JOptionPane.showMessageDialog(null, "Yoo"); //
+        // TO DO
+    }//GEN-LAST:event_jTable1keyTypedEvent
+
+    private void resetButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetButtonActionPerformed
+        tblModel.setRowCount(0);
+        retrieveData();
+        jTextField1.setText("");
+    }//GEN-LAST:event_resetButtonActionPerformed
+
+    private void jTextField1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyPressed
+        // TODO
+    }//GEN-LAST:event_jTextField1KeyPressed
+
+    private void jTextField1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyReleased
+        tblModel.setRowCount(0);
         String typedText = jTextField1.getText();
-        String q1 = "SELECT * FROM VALID_MODULES WHERE MODULE LIKE '%" + typedText + "%'";
+        String q1 = "SELECT * FROM VALID_MODULES WHERE MODULE LIKE ?";
         try {
             ps = con.prepareStatement(q1);
+            ps.setString(1, "%" + typedText.toUpperCase() + "%");
             rs = ps.executeQuery();
             while (rs.next()) {
                 String MODULES = rs.getString("MODULE");
                 String CREDIT = rs.getString("CREDIT");
                 String ACTIVITY = rs.getString("ACTIVITY");
-
                 String tbData[] = {MODULES, CREDIT, ACTIVITY};
-                DefaultTableModel tblModel = (DefaultTableModel) jTable1.getModel();
-                tblModel.setRowCount(0);
+
                 tblModel.addRow(tbData);
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e);
         }
-    }//GEN-LAST:event_jTable1keyTypedEvent
+
+        if (jTextField1.equals("")) {
+            retrieveData();
+        }
+    }//GEN-LAST:event_jTextField1KeyReleased
 
     /**
      * @param args the command line arguments
@@ -230,10 +253,10 @@ public class WelcomePage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton resetButton;
     // End of variables declaration//GEN-END:variables
 }
